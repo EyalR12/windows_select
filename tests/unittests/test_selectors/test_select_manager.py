808fc3df005de8cb3e1d4fe054f7412eb5d8efc9
Eyal Rosemarin <eyal.rosemarin@gmail.com>
2024-12-17 23:16:01 +0200

    fix test and ci

--- tests/unittests/test_selectors/test_select_manager.py
+++ tests/unittests/test_selectors/test_select_manager.py
@@ -16,7 +16,7 @@ def test_no_selectors(select_manager):
 
 def test_timeout(select_manager, mock_selector, select_timer):
     select_manager.timeout = 2
-    select_manager.add_selectors(mock_selector)
+    select_manager.add_selector(mock_selector)
     t = select_timer(select_manager.timeout + 1)
     t.start()
     select_manager.select_all()
